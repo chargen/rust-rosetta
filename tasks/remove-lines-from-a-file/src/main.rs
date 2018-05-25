22c68d89152303b2f250660fa597c7c0ee17947b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-25 15:21:46 +0000

    Deploy Hoverbear/rust-rosetta to github.com/Hoverbear/rust-rosetta.git:gh-pages

---
+++ tasks/remove-lines-from-a-file/src/main.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate docopt;
+extern crate serde;
+
+use docopt::Docopt;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const USAGE: &str = r"
+Usage: remove_lines_from_a_file <start> <count> <file>
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_start: usize,
+    arg_count: usize,
+    arg_file: String,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    let file = BufReader::new(File::open(args.arg_file).unwrap());
+
+    for (i, line) in file.lines().enumerate() {
+        let cur = i + 1;
+
+        if cur < args.arg_start || cur >= (args.arg_start + args.arg_count) {
+            println!("{}", line.unwrap());
+        }
+    }
+}
